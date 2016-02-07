c7848aed65b3ad45d86ea3c646a05912e4a7a22e
Peter Chapman <peter@conglomo.co.nz>
2016-02-07 18:23:53 +1300

    Fixed the config, added IE6 favicon support

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,18 +3,11 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'SS_RCPN',
-	'path' => ''
-);
+global $database;
+$database = 'SS_RCPN';
 
 // Set the site locale
 i18n::set_locale('en_GB');
 
-// Disable cache
-SS_Cache::set_cache_lifetime('any', -1, 100);
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
