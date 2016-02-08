588a9b1fed6dd9cf17435bd88885ee1fe6562ebf
Peter Chapman <peter@conglomo.co.nz>
2016-02-09 11:37:44 +1300

    Added the right sidebar template

---
+++ mysite/code/RightSidebarPage.php
@@ -0,0 +1,39 @@
+<?php
+class RightSidebarPage extends Page {
+
+	private static $has_one = array(
+		'TopSidebarUrl' => 'SiteTree',
+		'BottomSidebarUrl' => 'SiteTree'
+	);
+
+	private static $db = array(
+		'TopSidebarHeading' => 'Varchar(255)',
+		'TopSidebarContent' => 'Varchar(255)',
+		'BottomSidebarHeading' => 'Varchar(255)',
+		'BottomSidebarContent' => 'Varchar(255)',
+		'TopSidebarButton' => 'Varchar(20)',
+		'BottomSidebarButton' => 'Varchar(20)'
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		
+		// Add the top side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('TopSidebarSection', 'Top Right Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("TopSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+		
+		// Add the bottom side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('BottomSidebarSection', 'Bottom Right Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("BottomSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+
+		return $fields;
+	}
+}
+class RightSidebarPage_Controller extends Page_Controller {
+}
