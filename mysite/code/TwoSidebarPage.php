4807a072728d2c171b348de04a70d6c4976b43b7
Peter Chapman <peter@conglomo.co.nz>
2016-02-09 11:46:12 +1300

    Aded two sidebar template

---
+++ mysite/code/TwoSidebarPage.php
@@ -0,0 +1,61 @@
+<?php
+class TwoSidebarPage extends Page {
+
+	private static $has_one = array(
+		'TopSidebarUrl' => 'SiteTree',
+		'BottomSidebarUrl' => 'SiteTree',
+		'AltTopSidebarUrl' => 'SiteTree',
+		'AltBottomSidebarUrl' => 'SiteTree'
+	);
+
+	private static $db = array(
+		'TopSidebarHeading' => 'Varchar(255)',
+		'TopSidebarContent' => 'Varchar(255)',
+		'BottomSidebarHeading' => 'Varchar(255)',
+		'BottomSidebarContent' => 'Varchar(255)',
+		'TopSidebarButton' => 'Varchar(20)',
+		'BottomSidebarButton' => 'Varchar(20)',
+		'AltTopSidebarHeading' => 'Varchar(255)',
+		'AltTopSidebarContent' => 'Varchar(255)',
+		'AltBottomSidebarHeading' => 'Varchar(255)',
+		'AltBottomSidebarContent' => 'Varchar(255)',
+		'AltTopSidebarButton' => 'Varchar(20)',
+		'AltBottomSidebarButton' => 'Varchar(20)'
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		
+		// Add the top left side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('TopSidebarSection', 'Top Left Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('TopSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("TopSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+		
+		// Add the bottom left side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('BottomSidebarSection', 'Bottom Left Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('BottomSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("BottomSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+		
+		// Add the top right side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('AltTopSidebarSection', 'Top Right Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltTopSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltTopSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltTopSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("AltTopSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+		
+		// Add the bottom right side bar section
+		$fields->addFieldToTab('Root.Main', new HeaderField('AltBottomSidebarSection', 'Bottom Right Sidebar'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltBottomSidebarHeading', 'Heading'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltBottomSidebarContent', 'Content'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TextField('AltBottomSidebarButton', 'Button Caption'), 'Metadata');
+		$fields->addFieldToTab('Root.Main', new TreeDropdownField("AltBottomSidebarUrlID", "Link Button To Page", "SiteTree"), 'Metadata');
+
+		return $fields;
+	}
+}
+class TwoSidebarPage_Controller extends Page_Controller {
+}
---
+++ themes/arcana/templates/Layout/TwoSidebarPage.ss
@@ -0,0 +1,89 @@
+<!-- Main -->
+<section class="wrapper style1">
+	<div class="container">
+		<div class="row 200%">
+			<div class="3u 12u(narrower)">
+				<div id="sidebar1">
+					<!-- Sidebar 1 -->
+					<% if $TopSidebarHeading || $TopSidebarContent %>
+					<section>
+						<% if $TopSidebarHeading %>
+						<h3>$TopSidebarHeading</h3>
+						<% end_if %>
+						<% if $TopSidebarContent %>
+						<p>$TopSidebarContent</p>
+						<% end_if %>
+						<% if $TopSidebarButton && $TopSidebarUrl %>
+						<footer>
+							<a href="$TopSidebarUrl.Link" class="button">$TopSidebarButton</a>
+						</footer>
+						<% end_if %>
+					</section>
+					<% end_if %>
+					<% if $BottomSidebarHeading || $BottomSidebarContent %>
+					<section>
+						<% if $BottomSidebarHeading %>
+						<h3>$BottomSidebarHeading</h3>
+						<% end_if %>
+						<% if $BottomSidebarContent %>
+						<p>$BottomSidebarContent</p>
+						<% end_if %>
+						<% if $BottomSidebarButton && $BottomSidebarUrl %>
+						<footer>
+							<a href="$BottomSidebarUrl.Link" class="button">$BottomSidebarButton</a>
+						</footer>
+						<% end_if %>
+					</section>
+					<% end_if %>
+				</div>
+			</div>
+			<div class="6u 12u(narrower) important(narrower)">
+				<div id="content">
+					<!-- Content -->
+					<article>
+						<header>
+							<h2>$Title</h2>
+						</header>
+						$Content
+					</article>
+				</div>
+			</div>
+			<div class="3u 12u(narrower)">
+				<div id="sidebar2">
+					<!-- Sidebar 2 -->
+					<% if $AltTopSidebarHeading || $AltTopSidebarContent %>
+					<section>
+						<% if $AltTopSidebarHeading %>
+						<h3>$AltTopSidebarHeading</h3>
+						<% end_if %>
+						<% if $AltTopSidebarContent %>
+						<p>$AltTopSidebarContent</p>
+						<% end_if %>
+						<% if $AltTopSidebarButton && $AltTopSidebarUrl %>
+						<footer>
+							<a href="$AltTopSidebarUrl.Link" class="button">$AltTopSidebarButton</a>
+						</footer>
+						<% end_if %>
+					</section>
+					<% end_if %>
+					<% if $AltBottomSidebarHeading || $AltBottomSidebarContent %>
+					<section>
+						<% if $AltBottomSidebarHeading %>
+						<h3>$AltBottomSidebarHeading</h3>
+						<% end_if %>
+						<% if $AltBottomSidebarContent %>
+						<p>$AltBottomSidebarContent</p>
+						<% end_if %>
+						<% if $AltBottomSidebarButton && $AltBottomSidebarUrl %>
+						<footer>
+							<a href="$AltBottomSidebarUrl.Link" class="button">$AltBottomSidebarButton</a>
+						</footer>
+						<% end_if %>
+					</section>
+					<% end_if %>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<% include Footer %>
