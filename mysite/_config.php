aefcd831ed49df22a2c0ba6b159d40fb1b4a3607
Peter Chapman <peter@conglomo.co.nz>
2016-02-13 09:50:02 +1300

    Added editor stylesheet

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,5 +9,8 @@ $database = 'SS_RCPN';
 // Set the site locale
 i18n::set_locale('en_GB');
 
+// Set the editor stylesheet
+HtmlEditorConfig::get('cms')->setOption('content_css', project() . '/css/editor.css');
+
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
